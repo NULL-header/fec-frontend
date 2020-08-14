84b3bd9abe3ab41c4852de3dd86a369d5560d6be
NULL-header <poaoq.poaoq@gmail.com>
2020-08-14 15:59:40 +0000

    add: ToggleEyeIcon

---
+++ src/components/ToggleEyeIcon/index.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Eye from "@material-ui/icons/Visibility";
+import OffEye from "@material-ui/icons/VisibilityOff";
+
+import { DisplayContainer } from "../DisplayContainer";
+
+interface ToggleEyeIconProps extends BaseComponent {
+  currentName: string;
+  onClick: () => void;
+}
+
+const eyeMap = new Map([
+  ["show", Eye],
+  ["hidden", OffEye],
+]);
+
+export const ToggleEyeIcon: React.FC<ToggleEyeIconProps> = (props) => {
+  return (
+    <DisplayContainer componentMap={eyeMap} currentName={props.currentName} />
+  );
+};
