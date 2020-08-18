d968b565bacb9063d7853328e772caa917c6055f
NULL-header <poaoq.poaoq@gmail.com>
2020-08-18 08:14:51 +0000

    update: define type which must has key

--- src/dts/global.d.ts
+++ src/dts/global.d.ts
@@ -2,8 +2,14 @@ interface BaseComponentProps {
   className?: string;
 }
 
+interface KeyComponentProps extends BaseComponentProps {
+  key: string;
+}
+
 type BaseComponent = React.FC<BaseComponentProps>;
 
 type BaseElement = React.ReactElement<BaseComponentProps>;
 
-type ComponentMap = Map<string, BaseElement>;
+type KeyComponent = React.FC<KeyComponentProps>;
+
+type KeyElement = React.ReactElement<KeyComponentProps>;
