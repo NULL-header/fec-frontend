bb10623322421e891374c9f5b07bba5b3467b61a
NULL-header <poaoq.poaoq@gmail.com>
2020-08-18 16:41:08 +0000

    add: use useDoesMountEffect

---
+++ src/costomhook/doesMountEffect.ts
@@ -0,0 +1,18 @@
+// eslint-disable-next-line no-unused-vars
+import React, { useEffect, useRef } from "react";
+
+type effectFunc = () => Promise<unknown>;
+
+export const useDoesMountEffect = (
+  func: effectFunc,
+  deps: React.DependencyList | undefined
+) => {
+  const doesMount = useRef(false);
+
+  useEffect(() => {
+    if (doesMount.current) func();
+    else doesMount.current = true;
+    // func is a function, so this is not included deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, deps);
+};
