b9a50a4bedf185509eb778e47a0250ce501105dd
NULL-header <poaoq.poaoq@gmail.com>
2020-09-18 15:54:29 +0000

    add: test of RefPropertyControll

---
+++ tests/util/components/RefPropertyControll.test.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+// for to leave screen to debug
+// eslint-disable-next-line no-unused-vars
+import { render, screen } from "@testing-library/react";
+import { mock } from "jest-mock-extended";
+import "@testing-library/jest-dom";
+import { RefPropertyControll } from "../../../src/util/components";
+import { renderDomFactory } from "../../../src/util/test";
+import { getElementsFrom } from "../../../src/util/test/dom";
+
+describe("Normal system", () => {
+  const props = {
+    ref: mock<React.MutableRefObject<Record<"div", HTMLDivElement>>>(),
+    className: "testcase",
+    propertyName: "div",
+  };
+  const renderDom = renderDomFactory(
+    <RefPropertyControll {...props}>
+      <div />
+    </RefPropertyControll>,
+    props
+  );
+
+  it("add property", () => {
+    const {
+      props: { ref },
+    } = renderDom();
+    expect(ref.current).toHaveProperty("div");
+  });
+
+  it("pass ref", () => {
+    const {
+      props: { ref },
+    } = renderDom();
+    expect(ref.current.div).toHaveProperty("tagName", "DIV");
+  });
+
+  it("pass className", () => {
+    const className = "testClass";
+    const { container } = renderDom({ className });
+    const div = getElementsFrom(container).byTagName("div").asSingle();
+    expect(div.className).toEqual(className);
+  });
+});
