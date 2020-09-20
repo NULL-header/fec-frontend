a46942f2ba2d8421c569ab1cf5d0201ab59ed3c4
NULL-header <poaoq.poaoq@gmail.com>
2020-09-20 14:21:01 +0000

    add: types.d.ts

---
+++ src/util/types.d.ts
@@ -0,0 +1,20 @@
+// eslint-disable-next-line no-unused-vars
+import { MutableRefObject, ReactElement } from "react";
+
+export type Ref<T> =
+  | ((instance: T) => void)
+  | MutableRefObject<T | null>
+  | null;
+
+interface BaseComponentProps {
+  className?: string;
+}
+
+export interface HadChildComponentProps<T = BaseComponentProps>
+  extends BaseComponentProps {
+  children: ReactElement<T>;
+}
+
+interface RefComponentProps<T = unknown> extends BaseComponentProps {
+  ref: Ref<T>;
+}
