ba364eb10dbe5d59df3854f1be1ff80cfcca93e2
NULL-header <poaoq.poaoq@gmail.com>
2020-09-21 12:15:43 +0000

    add: operator of dom of util/test/dom

---
+++ src/util/test/dom/getElementsFrom.ts
@@ -0,0 +1,38 @@
+// Use this code when you write a test code, only.
+
+type Tags = keyof HTMLElementTagNameMap;
+
+const isLengthTruthy = (array: unknown[]) => array.length > 0;
+const checkLengthTruthy = (array: unknown[]) => {
+  if (!isLengthTruthy(array))
+    throw new Error("The Length of the array is zero");
+};
+
+// The class is for ElementSearcher.
+// Usually, it is returned as a search result.
+class Elements<T extends Tags> extends Array<HTMLElementTagNameMap[T]> {
+  constructor(maybeArray: HTMLCollectionOf<HTMLElementTagNameMap[T]>) {
+    super(...Array.from(maybeArray));
+    Object.setPrototypeOf(this, Elements.prototype);
+  }
+
+  asSingle() {
+    checkLengthTruthy(this);
+    if (this.length > 1)
+      throw new Error("Search result is more than number which you think");
+    return this[0];
+  }
+}
+
+class ElementSearcher<T extends Tags> {
+  constructor(private readonly container: HTMLElement) {}
+
+  byTagName(tagName: T) {
+    const els = this.container.getElementsByTagName(tagName);
+    return new Elements(els);
+  }
+}
+
+export const getElementsFrom = (
+  container = (document as unknown) as HTMLElement
+) => new ElementSearcher(container);
