e69364bec9a33416acd7706beaa0e55ac5dd64cc
NULL-header <poaoq.poaoq@gmail.com>
2020-09-22 06:24:49 +0000

    add: test of renderDomFactory

---
+++ tests/util/test/dom/renderDomFactory.test.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { renderDomFactory } from "src/util/test/dom";
+
+describe("Normal system", () => {
+  it("call function made without error", () => {
+    const renderDom = renderDomFactory(<div />);
+    expect(renderDom).not.toThrow();
+  });
+
+  it("render dom", () => {
+    const renderDom = renderDomFactory(<div />, { className: "testcase" });
+    const {
+      container,
+      props: { className },
+    } = renderDom();
+    const div = container.getElementsByTagName("div")[0];
+    expect(div).toHaveProperty("className", className);
+  });
+});
