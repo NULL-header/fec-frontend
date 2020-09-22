b80fc0287c9ddfbfca94f222e755218d02295169
NULL-header <poaoq.poaoq@gmail.com>
2020-09-22 06:17:18 +0000

    add: renderDomFactory of util #22

---
+++ src/util/test/dom/renderDomFactory.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { render } from "@testing-library/react";
+
+export const renderDomFactory = function <
+  T extends Record<string, unknown>,
+  U extends Partial<T>
+>(elements: React.ReactElement, getProps: () => T = () => ({} as T)) {
+  return (options = {} as U) => {
+    const props = getProps();
+    const passedElements = React.cloneElement(elements, {
+      ...props,
+      ...options,
+    });
+    const domTree = render(passedElements);
+    return { container: domTree.container, props: { ...props, ...options } };
+  };
+};
