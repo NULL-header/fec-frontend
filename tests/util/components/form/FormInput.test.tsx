a9e7987ad44a589edaacb758f76a8bfe4604099c
NULL-header <poaoq.poaoq@gmail.com>
2020-09-25 04:30:35 +0000

    add: test of FormInput #22

---
+++ tests/util/components/form/FormInput.test.tsx
@@ -0,0 +1,58 @@
+// ref RefPropertyControll.test.tsx, TextReader.test.tsx, WithValidate.test.tsx.
+// this target of test is maked with these.
+import React from "react";
+import { mock } from "jest-mock-extended";
+import "@testing-library/jest-dom";
+
+import { FormInput } from "src/util/components/form";
+// eslint-disable-next-line no-unused-vars
+import { ValidatedResult } from "src/util/types";
+import { getElementsFrom, renderDomFactory } from "src/util/test/dom";
+
+interface RefCurrent extends Record<string, () => ValidatedResult> {
+  testcase: () => ValidatedResult;
+}
+
+const getProps = () => ({
+  ref: mock<React.MutableRefObject<RefCurrent>>(),
+  propertyName: "testcase",
+  className: "testclass",
+});
+
+const renderDom = renderDomFactory(
+  <FormInput {...getProps()}>
+    <input />
+  </FormInput>,
+  getProps
+);
+
+const value = "testcase-input";
+const validatedResult = { value, isRegular: true };
+
+const getInput = (container: HTMLElement) =>
+  getElementsFrom(container).byTagName("input").asSingle();
+
+const setValueInput = (container: HTMLElement, settedValue: string) => {
+  const input = getInput(container);
+  input.value = settedValue;
+};
+
+describe("Normal system", () => {
+  it("pass Ref", () => {
+    const {
+      container,
+      props: { ref },
+    } = renderDom();
+    setValueInput(container, value);
+    expect(ref.current.testcase()).toEqual(validatedResult);
+  });
+
+  it("pass className", () => {
+    const {
+      container,
+      props: { className },
+    } = renderDom();
+    const input = getInput(container);
+    expect(input).toHaveProperty("className", className);
+  });
+});
