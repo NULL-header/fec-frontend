d6ade271fd2fc7aa7d0cb89abee0b223385c3101
NULL-header <poaoq.poaoq@gmail.com>
2020-09-25 03:06:02 +0000

    add: test of FormLabel #22

---
+++ tests/util/components/form/FormLabel.test.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { FormLabel } from "src/util/components/form";
+import { getElementsFrom, renderDomFactory } from "src/util/test/dom";
+
+const getProps = () => ({
+  className: "testclass",
+  ref: jest.fn(),
+});
+
+const renderDom = renderDomFactory(
+  <FormLabel {...getProps()}>
+    <div />
+  </FormLabel>,
+  getProps
+);
+
+describe("Normal system", () => {
+  it("pass className", () => {
+    const {
+      container,
+      props: { className },
+    } = renderDom();
+    const div = getElementsFrom(container).byTagName("div").asSingle();
+    expect(div).toHaveProperty("className", className);
+  });
+
+  it("pass no ref", () => {
+    const {
+      props: { ref },
+    } = renderDom();
+    expect(ref).not.toBeCalled();
+  });
+});
