669e24f20bfc8937b5009bad083ebe15195923c2
NULL-header <poaoq.poaoq@gmail.com>
2020-09-27 19:59:16 +0000

    add: useIsMounted of util #22

---
+++ src/util/customhook/useIsMounted.ts
@@ -0,0 +1,14 @@
+import { useCallback, useEffect, useRef } from "react";
+
+export const useIsMounted = () => {
+  const mountedRef = useRef(false);
+
+  useEffect(() => {
+    mountedRef.current = true;
+    return () => {
+      mountedRef.current = false;
+    };
+  }, []);
+
+  return useCallback(() => mountedRef.current, []);
+};
