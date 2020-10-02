2f862bbf95b7897aafb2ad5502a7e49a0386a438
NULL-header <poaoq.poaoq@gmail.com>
2020-10-02 13:30:29 +0000

    add: validate of ligics #22

---
+++ src/logics/validates/index.ts
@@ -0,0 +1,4 @@
+export * as EmailValidate from "./emailValidate";
+export * as NameValidate from "./nameValidate";
+export * as PasswordValidate from "./passwordValidate";
+export * from "./helper";
