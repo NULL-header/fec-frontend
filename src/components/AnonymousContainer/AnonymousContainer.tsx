5a20608429099ab6bedda4cbd11a168d655b1f6c
NULL-header <poaoq.poaoq@gmail.com>
2020-10-04 17:45:14 +0000

    update: use SideBar #35

--- src/components/AnonymousContainer/AnonymousContainer.tsx
+++ src/components/AnonymousContainer/AnonymousContainer.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 
+import { SideBar } from "src/components";
+
 const Component: React.FC<BaseComponentProps> = (props) => {
-  return <div>AnonymousContainer</div>;
+  return (
+    <div>
+      <SideBar />
+      <div>AnonymousContainer</div>
+    </div>
+  );
 };
 
 const AnonymousContainer = React.memo(Component);
