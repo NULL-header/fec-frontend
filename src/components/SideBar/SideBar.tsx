e6c006203157da3f8bfdf674a6be03d010eecb03
NULL-header <poaoq.poaoq@gmail.com>
2020-10-10 07:39:32 +0000

    update: apply CSS #35

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -1,10 +1,29 @@
 import React from "react";
+import withStyles from "react-jss";
+import { styles } from "./style";
 
-const Component: React.FC<BaseComponentProps> = (props) => {
-  return <div>sidebar</div>;
+interface Props extends BaseComponentProps {
+  classes: Record<keyof typeof styles, string>;
+  setValues: (e: string) => void;
+  children: React.ReactElement[];
+}
+
+const Component: React.FC<Props> = (props) => {
+  const onClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    e.preventDefault();
+    const button = e.currentTarget;
+    props.setValues(button.value);
+  };
+  const buttons = React.Children.map(props.children, (e, i) =>
+    React.cloneElement(e, { onClick, className: props.classes.button })
+  );
+
+  return (
+    <div className={props.classes.root + " " + props.className}>{buttons}</div>
+  );
 };
-
-const SideBar = React.memo(Component);
+<button />;
+const SideBar = React.memo(withStyles(styles as any)(Component));
 SideBar.displayName = "SideBar";
 
 export { SideBar };
