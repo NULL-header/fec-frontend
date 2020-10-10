35f88509f7c46dc24ac85be34d0df8632243b4ba
NULL-header <poaoq.poaoq@gmail.com>
2020-10-10 07:39:32 +0000

    update: apply CSS #35

--- src/components/AnonymousContainer/AnonymousContainer.tsx
+++ src/components/AnonymousContainer/AnonymousContainer.tsx
@@ -1,17 +1,35 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
+import withStyle from "react-jss";
 
-import { SideBar } from "src/components";
+import { SideBar, DisplayContainer } from "src/components";
+import { styles } from "./style";
 
-const Component: React.FC<BaseComponentProps> = (props) => {
+interface Props extends BaseComponentProps {
+  classes: Record<keyof typeof styles, string>;
+}
+
+const Component: React.FC<Props> = (props) => {
+  const [[currentName], setStates] = useState(["Home"]);
+
+  const insertState = useCallback((e: string) => setStates([e]), []);
   return (
-    <div>
-      <SideBar />
-      <div>AnonymousContainer</div>
+    <div className={props.classes.root}>
+      <SideBar setValues={insertState} className={props.classes.sidebar}>
+        <button value="Home">Home</button>
+        <button value="Search">Search</button>
+        <button value="MyPage">MyPage</button>
+      </SideBar>
+      <div style={{ width: "50%" }}>AnonymousContainer</div>
+      <DisplayContainer currentKey={currentName}>
+        <div key="Home">Home</div>
+        <div key="Search">Search</div>
+        <div key="MyPage">MyPage</div>
+      </DisplayContainer>
     </div>
   );
 };
 
-const AnonymousContainer = React.memo(Component);
+const AnonymousContainer = React.memo(withStyle(styles as any)(Component));
 AnonymousContainer.displayName = "AnonymousContainer";
 
 export { AnonymousContainer };
---
+++ src/components/SideBar/style.ts
@@ -0,0 +1,12 @@
+import { makeStyles } from "src/util";
+
+export const styles = makeStyles({
+  root: {
+    backgroundColor: "darkgreen",
+    display: "flex",
+    flexDirection: "column",
+  },
+  button: {
+    margin: "30% 10%",
+  },
+});
