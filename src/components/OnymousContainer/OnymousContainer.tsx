d67309737bd410a4a2da8ae9f95c7c998a4edb17
NULL-header <poaoq.poaoq@gmail.com>
2020-10-10 07:39:59 +0000

    update: for css like anonymouscontainer #35

--- src/components/OnymousContainer/OnymousContainer.tsx
+++ src/components/OnymousContainer/OnymousContainer.tsx
@@ -1,10 +1,42 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
+import withStyle from "react-jss";
 
-const Component: React.FC<BaseComponentProps> = (props) => {
-  return <div>OnymousContainer</div>;
+import { SideBar, DisplayContainer } from "src/components";
+import { useVariable } from "src/util/customhook";
+import { styles } from "./style";
+
+interface Props extends BaseComponentProps {
+  classes: Record<keyof typeof styles, string>;
+}
+
+const Component: React.FC<Props> = (props) => {
+  const [[currentName], setStates] = useState(["Home"]);
+  const classes = useVariable(props.classes);
+
+  const insertState = useCallback((e: string) => setStates([e]), []);
+
+  return (
+    <div className={classes.root}>
+      <SideBar setValues={insertState} className={classes.sidebar}>
+        <button value="Home">Home</button>
+        <button value="Search">Search</button>
+        <button value="Notify">Notify</button>
+        <button value="MyPage">MyPage</button>
+        <button value="Option">Option</button>
+      </SideBar>
+      <div style={{ width: "50%" }}>OnymousContainer</div>;
+      <DisplayContainer currentKey={currentName}>
+        <div key="Home">Home</div>
+        <div key="Search">Search</div>
+        <div key="Notify">Notify</div>
+        <div key="MyPage">MyPage</div>
+        <div key="Option">Option</div>
+      </DisplayContainer>
+    </div>
+  );
 };
 
-const OnymousContainer = React.memo(Component);
+const OnymousContainer = React.memo(withStyle(styles as any)(Component));
 OnymousContainer.displayName = "OnymousContainer";
 
 export { OnymousContainer };
---
+++ src/components/OnymousContainer/style.ts
@@ -0,0 +1,11 @@
+import { makeStyles } from "src/util";
+
+export const styles = makeStyles({
+  root: {
+    display: "flex",
+  },
+  sidebar: {
+    height: "100vh",
+    width: "100px",
+  },
+});
