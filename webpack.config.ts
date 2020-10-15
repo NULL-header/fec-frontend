b315b4a34ff30ae5075ddc461b2ef86422255c55
NULL-header <poaoq.poaoq@gmail.com>
2020-10-15 06:05:03 +0000

    fix: bugs with develop #50

--- webpack.config.ts
+++ webpack.config.ts
@@ -57,9 +57,10 @@ const webpackConfig = (env: Env): webpack.Configuration => ({
       "process.env.PRODUCTION": env.master || !(env.develop || env.feature),
       "process.env.NAME": JSON.stringify(packageJSON.name),
       "process.env.VERSION": JSON.stringify(packageJSON.version),
-      "process.env.BRANCH_NAME": JSON.stringify(
-        encodeURIComponent(env.branchName)
-      ),
+      "process.env.BRANCH_NAME":
+        env.branchName == null
+          ? undefined
+          : JSON.stringify(encodeURIComponent(env.branchName)),
     }),
     new ForkTsCheckerWebpackPlugin(),
     new Dotenv({
