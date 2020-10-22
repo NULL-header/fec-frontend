7f8db279335cebd26e7285e4495c1380317c56c5
NULL-header <poaoq.poaoq@gmail.com>
2020-10-22 13:14:44 +0000

    update: without call before rendering #62

--- src/components/AnonymousContainer/AnonymousContainer.tsx
+++ src/components/AnonymousContainer/AnonymousContainer.tsx
@@ -14,6 +14,13 @@ interface Props extends BaseComponentProps {
   classes: Record<keyof typeof styles, string>;
 }
 
+const components = {
+  "/home": () => <div>Home</div>,
+  "/search": () => <div>Search</div>,
+  "/login": LoginContainer,
+  "/signup": SignupContainer,
+} as Record<string, React.FC>;
+
 const Component: React.FC<Props> = (props) => {
   return (
     <div className={props.classes.root}>
@@ -23,12 +30,7 @@ const Component: React.FC<Props> = (props) => {
         <LinkButton to="login">Log in</LinkButton>
         <LinkButton to="signup">Sign up</LinkButton>
       </SideBar>
-      <SwitchContainer>
-        <div key="/home">Home</div>
-        <div key="/search">Search</div>
-        <LoginContainer key="/login" />
-        <SignupContainer key="/signup" />
-      </SwitchContainer>
+      <SwitchContainer components={components} />
     </div>
   );
 };
