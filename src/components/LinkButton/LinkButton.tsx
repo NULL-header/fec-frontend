734eed293d5686f8780d89113a782310b97b61ef
NULL-header <poaoq.poaoq@gmail.com>
2020-10-28 03:47:09 +0000

    update: for nested routing #76

--- src/components/LinkButton/LinkButton.tsx
+++ src/components/LinkButton/LinkButton.tsx
@@ -1,14 +1,19 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { Link } from "react-router-dom";
 
 interface Props extends BaseComponentProps {
   to: string;
+  base?: string;
   children: React.ReactNode;
 }
 
 const Component: React.FC<Props> = (props) => {
+  const url = useMemo(() => (props.base == null ? "" : props.base) + props.to, [
+    props.base,
+    props.to,
+  ]);
   return (
-    <Link to={props.to}>
+    <Link to={url}>
       <button>{props.children}</button>
     </Link>
   );
