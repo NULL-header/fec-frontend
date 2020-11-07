eebfa2525f455fafe290ee8219221418fd251ce9
NULL-header <poaoq.poaoq@gmail.com>
2020-11-07 03:15:02 +0000

    update: use theme #94

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,20 @@
-import React from "react";
+import React, { useMemo } from "react";
+import { BrowserRouter } from "react-router-dom";
 
 import { AppContainer } from "./components";
+import { CONSTVALUES } from "./config";
+import { theming } from "./theme";
 
-export const App = () => <AppContainer />;
+const { ThemeProvider, useTheme } = theming;
+
+export const App = () => {
+  const baseName = useMemo(() => CONSTVALUES.routeUrl, []);
+  const theme = useTheme();
+  return (
+    <ThemeProvider theme={theme}>
+      <BrowserRouter basename={baseName}>
+        <AppContainer />
+      </BrowserRouter>
+    </ThemeProvider>
+  );
+};
