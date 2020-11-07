970b125e4b5438a5262485aa53c72c7dc321a1b2
NULL-header <poaoq.poaoq@gmail.com>
2020-11-07 03:03:45 +0000

    update: use createUseStyles #94

--- src/util/makeStyles.ts
+++ src/util/makeStyles.ts
@@ -1,13 +1,28 @@
 // eslint-disable-next-line no-unused-vars
 import { CSSProperties } from "react";
+// eslint-disable-next-line no-unused-vars
+import { createUseStyles, Theming } from "react-jss";
 
-type MassJss<T> = {
-  [Key in keyof T]: T[Key] extends CSSProperties
-    ? CSSProperties
-    : Key extends keyof CSSProperties
-    ? CSSProperties[Key]
-    : MassJss<T[Key]>;
+type MassJss<T, U> = {
+  [Key in keyof T]: Key extends keyof CSSProperties
+    ? CSSProperties[Key] | ((props: U) => CSSProperties[Key])
+    : CSSProperties | MassJss<T[Key], U>;
 };
 
-export const makeStyles = <T extends Record<string, any>>(styles: MassJss<T>) =>
-  styles;
+type Branch<T, U> = T extends undefined
+  ? () => Record<keyof U, string>
+  : (arg: T) => Record<keyof U, string>;
+
+export const makeStyles = <U extends Theming<any> | undefined = undefined>(
+  theming?: U
+) => <T extends Record<string, any> | undefined = undefined>(props?: T) => <
+  V extends Record<string, any>
+>(
+  styles: U extends undefined
+    ? MassJss<V, T>
+    :
+        | MassJss<V, T>
+        | ((theme: U extends Theming<infer W> ? W : never) => MassJss<V, T>)
+) => {
+  return createUseStyles<U>(styles as any, { theming }) as Branch<T, V>;
+};
