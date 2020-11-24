6660d1acf91aae19edfa95cfcfb73cac08258a2c
NULL-header <poaoq.poaoq@gmail.com>
2020-11-24 12:44:09 +0900

    update: add DbProvider #109

--- src/app.tsx
+++ src/app.tsx
@@ -2,16 +2,19 @@ import React, { useMemo } from "react";
 import { BrowserRouter } from "react-router-dom";
 
 import { ThemeProvider } from "./theme";
+import { DbProvider } from "./db";
 import { AppContainer } from "./components";
 import { CONSTVALUES } from "./config";
 
 export const App = () => {
   const baseName = useMemo(() => CONSTVALUES.routeUrl, []);
   return (
-    <ThemeProvider>
-      <BrowserRouter basename={baseName}>
-        <AppContainer />
-      </BrowserRouter>
-    </ThemeProvider>
+    <DbProvider>
+      <ThemeProvider>
+        <BrowserRouter basename={baseName}>
+          <AppContainer />
+        </BrowserRouter>
+      </ThemeProvider>
+    </DbProvider>
   );
 };
