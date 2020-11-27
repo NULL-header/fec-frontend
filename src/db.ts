395095bf60e0c007862e57940b5f207d48eea104
NULL-header <poaoq.poaoq@gmail.com>
2020-11-27 15:06:40 +0900

    update: adjust tables #111

--- src/db.ts
+++ src/db.ts
@@ -2,8 +2,8 @@ import { DbWrapper } from "./util/IndexedDbWrapper";
 import { createUsefulDb } from "./util/UsefulDb";
 
 const dbWrapped = new DbWrapper().addTable(
-  { configTheme: "themeName" },
-  {} as { themeName: string }
+  { configTheme: "++, themeName" },
+  {} as { themeName: "light" | "dark" | "loading" }
 );
 
 export const { DbProvider, useDb } = createUsefulDb(dbWrapped);
