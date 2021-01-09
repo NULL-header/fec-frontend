5cc081a3344ef34f65569da971e63b1a8f7ce5fd
NULL-header <poaoq.poaoq@gmail.com>
2021-01-09 05:27:00 +0000

    update: adjust for added modules #113

--- src/app.tsx
+++ src/app.tsx
@@ -1,17 +1,49 @@
-import React, { useMemo } from "react";
+import React, { useCallback } from "react";
 import { BrowserRouter } from "react-router-dom";
 
-import { DbProvider } from "./db";
 import { AppContainer } from "./components";
 import { CONSTVALUES } from "./config";
+import { Provider, useUpdate, KVSState, useSelector } from "./globalState";
+import { useTheme } from "./theme";
+
+const ThemeLoader: HavingChildComponent = React.memo((props) => {
+  const isLoading = useSelector((state) => state.isLoadingThemeName);
+  const kvs = useSelector((state) => state.kvs);
+  const themeName = useSelector((state) => state.themeName);
+  const dispatch = useUpdate();
+  const setStateKVS = useCallback(
+    (args: KVSState) => dispatch({ type: "KVS", states: args }),
+    [dispatch]
+  );
+  const ThemeProvider = useTheme(
+    CONSTVALUES.themeNameCacheKey,
+    kvs,
+    CONSTVALUES.defaultThemeName,
+    setStateKVS
+  );
+  if (isLoading == null) return <div>Start loading theme name</div>;
+  else if (isLoading) return <div>Loading theme name</div>;
+  else
+    return (
+      <ThemeProvider themeName={themeName}>{props.children}</ThemeProvider>
+    );
+});
+
+const DBLoader: HavingChildComponent = React.memo((props) => {
+  const isLoading = useSelector((state) => state.isLoadingDB);
+  if (isLoading == null) return <div>Start loading</div>;
+  else if (isLoading) return <div>Loading DB</div>;
+  else return <ThemeLoader>{props.children}</ThemeLoader>;
+});
 
 export const App = () => {
-  const baseName = useMemo(() => CONSTVALUES.routeUrl, []);
   return (
-    <DbProvider>
-      <BrowserRouter basename={baseName}>
-        <AppContainer />
+    <Provider>
+      <BrowserRouter basename={CONSTVALUES.routeUrl}>
+        <DBLoader>
+          <AppContainer />
+        </DBLoader>
       </BrowserRouter>
-    </DbProvider>
+    </Provider>
   );
 };
